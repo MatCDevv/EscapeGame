63c9e204453e194103736dae520768e864db0006
MatCDevv <chapmatpr@gmail.com>
2024-04-30 10:06:24 +0200

    LastSave 10H05 30/04/24

--- script.js
+++ script.js
@@ -6,11 +6,34 @@ fetch("escapeGame.json")
     addProducts(data);
   });
 
-function addProducts (data) 
-  { data.forEach ((element) => {
-    let acti=""
-    element.activites.forEach ((a) => { 
-      (acti+=`<h4>${a.nom}</h4>`);
-    })
-  })
-};miuhliuhi
+  function addProducts(data) {
+    let activi = "";
+    data.entreprise.activites.forEach((a) => {
+      activi += `<li>${a.nom} ${a.description} <img src="${a.image}" alt="${a.nom}"/> </li>`;
+    });
+    let pres = "";
+    data.entreprise.presentation.forEach((p) => {
+      pres += `<li>${p.titre} ${p.texte} <img src="${p.image}" alt=""/> </li>`;
+    });
+    let temoin = "";
+    data.entreprise.temoignages.forEach((t) => {
+      temoin += `<li>${t.prenom} ${t.typeExperience} ${t.commentaire}${t.note}</li>`;
+    });
+    document.querySelector("#content").innerHTML += `
+    <section class="">
+    <video autoplay muted loop id="Video">
+    <source src="" type="video/mp4">
+    </video>
+    <div class="">
+      <strong> ${data.entreprise.nomCommercial}</strong>
+      <h1 >${data.entreprise.phraseAccroche}</h1>
+      </div>
+      </section>
+      ${data.entreprise.texteAppelAction}
+      ${data.entreprise.avantagesClients}
+      ${activi}
+      ${pres}
+      ${temoin}
+    `;
+    console.log(data);
+  }
