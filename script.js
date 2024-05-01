6fd5fbef9c44e38d1b7ccfec20b0c3c2c59cdd2a
MatCDevv <chapmatpr@gmail.com>
2024-05-01 18:16:01 +0200

    LastSave 18H15 01/05/24

--- script.js
+++ script.js
@@ -12,23 +12,34 @@ fetch("escapeGame.json")
     });
     let pres = "";
     data.entreprise.presentation.forEach((p) => {
-      pres += `<li>${p.titre} ${p.texte} <img src="${p.image}" alt=""/> </li>`;
+      pres += `
+      <article class="articlep flex spacebettween enligne">
+      <div class="width50">
+      <img class="cardimgp" src="${p.image}" alt="Images des Présentations"/>
+      </div>
+   
+      <div class="width50">
+      <h3 class="cardtp">${p.titre}</h3>
+      <p class="cardtexp">${p.texte}</p>
+      </div>
+      </article>`;
     });
     let temoin = "";
     data.entreprise.temoignages.forEach((t) => {
       temoin += `<li>${t.prenom} ${t.typeExperience} ${t.commentaire}${t.note}</li>`;
     });
-    document.querySelector(".content").innerHTML += `
+    document.querySelector(".tetedepage").innerHTML += `
     <div>
         <h1 class="nom">${data.entreprise.nomCommercial}</h1>
         <p class="pp">${data.entreprise.phraseAccroche}</p>
     </div>`;
-    document.querySelector(".con").innerHTML += `
-    <p class="textconcept"><span class="material-symbols-outlined guille">format_quote</span>  
-    Un escape game est un jeu d'énigmes qui se vit en équipe. Les joueurs évoluent généralement dans un lieu clos et thématisé. Ils doivent résoudre une série de casse-têtes dans un temps imparti pour réussir à s'échapper ou à accomplir une mission.
-    <span class="material-symbols-outlined guille">format_quote</span> </p>
-    <a href="./Réserver.html" alt="Réserver" class="boutonreser"><span class="material-symbols-outlined">touch_app</span></a>
+    document.querySelector(".appelreserv").innerHTML += `
+    <p class="appelres">${data.entreprise.texteAppelAction}</p>
     `;
+    document.querySelector(".prestaj").innerHTML += `
+    <section class="prestaj container-1000">${pres}</section>`;
+    
+    
     console.log(data);
   }
 
